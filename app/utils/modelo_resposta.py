f614e070fcb33a137e7fb828468e7fa8f8fab193
Marcos Henrique <marcosfhtrabalho@gmail.com>
2025-06-21 10:21:41 -0300

    feat: adição de um modelo de resposta padrão para a api

--- app/utils/modelo_resposta.py
+++ app/utils/modelo_resposta.py
@@ -1,8 +1,18 @@
 
-def reposta_api(dado = None, mensagem = "Sucesso", status = True, erro = None):
+def resposta_api(dados = None, mensagem_sistema = "Sucesso", status_code = 404, erro = None):
+    '''Resposta padrão da api para quem enviou a requisição
+    
+    Args:
+        dados: Representa a resposta final para o usuário da api.
+        mensagem_sistema: Representa a mensagem do sistema em relação à requisição.
+        status_code: Representa uma abstração do código HTTP para o frontend.
+
+    
+    '''
+
     return {
-        "status": status,
-        "mensagem": mensagem,
-        "dado": dado,
+        "status": status_code,
+        "mensagem": mensagem_sistema,
+        "dado": dados,
         "erro": erro
     }
