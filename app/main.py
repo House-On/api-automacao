8a8fc259d6df6e7ada4c313876dc591f8b4d38da
Marcos Henrique <marcosfhtrabalho@gmail.com>
2025-06-21 10:25:32 -0300

    feat: conecta as rotas do controller com a aplicação principal

--- app/main.py
+++ app/main.py
@@ -1,31 +1,32 @@
 
+import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from contextlib import asynccontextmanager
 from app.db.database import Base, engine
-import os
 from app.models.Analise import Analise
 from app.models.Empresa import Empresa
 from app.models.Historico import Historico
 from app.models.Imagens_buffer import Imagens
 
+from app.controllers.chatbot_controller import router as chatbot_router
+
 # Carrega as variáveis do .env
 load_dotenv()
 
-# Para criar e dar drop no banco toda vez que iniciar o backend
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     if os.getenv("ENV") == "dev":
-        print("[DEV MODE] Resetando banco de dados...")
-        Base.metadata.drop_all(bind=engine)
+        print("[DEV MODE] Verificando se banco precisa ser criado...")
         Base.metadata.create_all(bind=engine)
     yield
 
+# Instancia a aplicação principal e define uma regra de criação do banco antes do início da mesma.
 app = FastAPI(lifespan=lifespan)
 
-# Requisição raiz do main
+# Inclui as rotas do chatbot controller na aplicação principal
+app.include_router(chatbot_router, prefix="/chat", tags=["Chats"])
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
